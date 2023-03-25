af347a3d9e12b63a0f190d1bfe3fcfa8d099b229
Lian Liu <riceudon@gmail.com>
2023-03-25 14:00:04 -0700

    Added some components to the app

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <img src="https://lh3.googleusercontent.com/sXpaX3pdPww4EBzIB0kJgc4o5wfvdFLD3SiBRUEQdzXqg8w8XnT4oTfrfAYWMApfuJWx3DhcmWb4zj_BBFFWWCwZ6Aves03zHPCAOBex8gmEE0gTWpPI42f4EtifwyUhyXiLl5ce8vbUsNVRrHE7HLV0PnKMPTW3L2lrDx-w6wPgYdjDMOyUYEMIOidbqzUgVsYeRRVO-0l5srtJPYZB0ao24hiuRVCVBdOhvngoyluZXHOQ8zUo9MiO4uEKPkAvHSEPWzw_Vy77lG-mlA49pTvI4PahuSI0eLIBft8VTedZuMKi09Yws0U4l-GvkJ61jVopcryPuz9wI13_U7goNbHvTa4eTRukhjczxAvVsSWaTVRVGcPqu5a8MIxzHro_G50Kmji5w4QhaFLi3PSERnOJUanB2a0IteBfkKVqmROU9BwBQKKoleBSNgXC_ULjjfpvZFgq89ph-8JxZwkHUIrquk9MieXcE2R369usxLOaM-O8rC5sRJsI_zRW80VSvhJjn6Q6OK1e-LjNJdHfm_uKv54tqE9G4Bkc6psBtDTV-msiq7-v19AQAzRJp1XK3YhtqD7-cseU7Dtf9seGvkvbLmw7NmVBK-NbW4gJoZbzznkHkHCVhiJz8eHxAjKOhCkLCgYx-OjrEpJL0PDMcrq-1S2BTwID7-CIrN42zggRUxAZaik73tMwDKg6GW1d2_kPxqfXVeGaq1pWPJlm_b2wZ-Bf4qMBVC279dZ1HsauAt5OL4ht1pTUfxy3A0jg4Ifa22jP9yk678NPmJYc9ZrJq9k9KzBG-IVFQ66fCCyVq_xGep7Uj_Fsei1SR5ieJl3BJWxfQ-ldMcDU21uRSd735Hj9_m2sL3qNLoDU-DN0qewKZCDdewy4N9EKZK6p5CJGiQpY8Lp6ijMmZTjKs9LYw_ZyjR9FhcqC-M-kYdRNxYq-=w727-h969-no?authuser=0" alt="Photo with my daughter" className="my-2" style={{ width: "100%" }} />
+            <p>
+                Hello, My name is Lian. I used to be a math tutor and I love all kinds of logic
+                challenges.
+                After I found that coding coould be really challenging and logical, so I wanna give it a
+                try.
+                It turns out to be my hit! I started from a bootcamp and this is my portfolio with all my projects so far.
+                You can navigate to my projects by clicking the links on the top of the page.
+            </p>
+        </div>
+    );
+}
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio!</h1>
+      <p>
+        Feel free to look around and get to know me. I'm a full stack web developer with a passion for learning and creating. I'm currently looking for a full time position as a web developer. I'm also open to freelance work. If you have any questions or would like to work together, please contact me.
+      </p>
+    </div>
+  );
+}
